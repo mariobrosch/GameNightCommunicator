83574e6f3a0c46115af95fb05408fe679de28c13
Mario <info@brosch.nl>
2020-01-18 17:08:08 +0100

    First version to send and receive data from a hard coded COM port

---
+++ GameNightSerialCommunications/MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace GameNightSerialCommunications
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtCommand = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.txtDataReceived = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(376, 12);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Verstuur";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtCommand
+            // 
+            this.txtCommand.Location = new System.Drawing.Point(12, 14);
+            this.txtCommand.Name = "txtCommand";
+            this.txtCommand.Size = new System.Drawing.Size(358, 20);
+            this.txtCommand.TabIndex = 0;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // txtDataReceived
+            // 
+            this.txtDataReceived.Location = new System.Drawing.Point(12, 62);
+            this.txtDataReceived.Multiline = true;
+            this.txtDataReceived.Name = "txtDataReceived";
+            this.txtDataReceived.Size = new System.Drawing.Size(439, 364);
+            this.txtDataReceived.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSend;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDataReceived);
+            this.Controls.Add(this.txtCommand);
+            this.Controls.Add(this.btnSend);
+            this.Name = "MainForm";
+            this.Text = "Family Game Night Communicator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtCommand;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox txtDataReceived;
+    }
+}
+
---
+++ GameNightSerialCommunications/MainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GameNightSerialCommunications
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            serialPort1.BaudRate = 9600;
+            serialPort1.PortName = "COM5";
+            serialPort1.Open();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+
+            serialPort1.WriteLine(txtCommand.Text);
+            txtCommand.Text = "";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            serialPort1.Close();
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string line = serialPort1.ReadLine();
+            setDataReceived(line);
+
+        }
+
+        private void setDataReceived(string dataReceived)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(setDataReceived), new object[] { dataReceived });
+                return;
+            }
+            txtDataReceived.AppendText(dataReceived + Environment.NewLine);
+        }
+    }
+}
